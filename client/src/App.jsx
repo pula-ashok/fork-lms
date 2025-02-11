024f5a67006e49b5f16eeec57fdca60b28a9da65
pula-ashok <ashokpula0307@gmail.com>
2025-02-11 22:49:03 +0530

    created routes for student and educator

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/student/Home';
+import CoursesList from './pages/student/CoursesList';
+import CourseDetails from './pages/student/CourseDetails';
+import Player from './pages/student/Player';
+import Loading from './components/student/Loading';
+import Educator from './pages/educator/Educator';
+import Dashboard from './pages/educator/Dashboard';
+import AddCourse from './pages/educator/AddCourse';
+import MyCourses from './pages/educator/MyCourses';
+import StudentsEnrolled from './pages/educator/StudentsEnrolled';
 const App = () => {
   return (
-    <div className="bg-blue-500">App</div>
+    <di>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path="/course-list" element={<CoursesList/>} />
+        <Route path="/course-list/:input" element={<CoursesList/>} />
+        <Route path="/course/:id" element={<CourseDetails/>} /> 
+        <Route path='/player/:courseId' element={<Player/>}/>
+        <Route path='/loading/:path' element={<Loading/>}/>
+        <Route path='/educator' element={<Educator/>}>
+          <Route path='dashboard' element={<Dashboard/>}/>
+          <Route path='add-course' element={<AddCourse/>}/>
+          <Route path='my-courses' element={<MyCourses/>}/>
+          <Route path='students-enrolled' element={<StudentsEnrolled/>}/>
+        </Route>
+      </Routes>
+    </di>
   )
 }
 
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Educator = () => {
   return (
-    <div>Educator</div>
+    <div>
+        <div>Educator</div>
+        <Outlet/>
+    </div>
   )
 }
 
