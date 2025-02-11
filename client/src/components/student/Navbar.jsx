48db720f06423bacc53330384c5a38a6e1f63623
pula-ashok <ashokpula0307@gmail.com>
2025-02-12 00:09:42 +0530

    initial clerks setup done

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import { assets } from './../../assets/assets';
 import { Link } from 'react-router-dom';
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react"
+
 
 const Navbar = () => {
   const isCourseListpage = location.pathname.includes("/course-list");
+  const {openSignIn} = useClerk();
+  const {user} = useUser();
   return (
     <div className={`flex justify-between items-center px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListpage ? "bg-white":"bg-cyan-100/70"}`}>
       <img src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer'/>
       {/* desktop view  */}
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
-        <div className='flex items-center gap-5'>
+        {user &&<div className='flex items-center gap-5'>
           <button>Become Educator</button>
           | <Link to={"/my-enrollments"}>My Enrollements</Link>
-        </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+        </div>}
+        {user ? <UserButton/> : <button className='bg-blue-600 text-white px-5 py-2 rounded-full' onClick={()=>openSignIn()}>Create Account</button>}
       </div>
       {/* mobile view  */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
-        <div className='flex items-center gap-5'>
+        {user && <div className='flex items-center gap-5'>
           <button>Become Educator</button>
           | <Link to={"/my-enrollments"}>My Enrollements</Link>
-        </div>
-        <button><img src={assets.user_icon} alt='user icon'/></button>
+        </div>}
+        {user ? <UserButton/> :<button onClick={()=>openSignIn()}><img src={assets.user_icon} alt='user icon'/></button>}
       </div>
     </div>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,19 @@ import "./index.css";
 import App from "./App.jsx";
 import AppContextProvider from "./context/AppContext.jsx";
 import {BrowserRouter} from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react"
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+console.log("first",PUBLISHABLE_KEY)
+if(!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={"/"}>
     <AppContextProvider>
-      <App />
+     <App />
     </AppContextProvider>
+   </ClerkProvider>
   </BrowserRouter>
 );
