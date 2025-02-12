66497b25e0cd4bc3d438e3a07c9498007437d67c
pula-ashok <ashokpula0307@gmail.com>
2025-02-12 11:32:10 +0530

    hero section started

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { assets } from './../../assets/assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70'>
+      <h1 className='md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto'>Empower your future with the courses designed to <span className='text-blue-600'>fit your choice.</span><img src={assets.sketch} alt="sketch" className='hidden md:block absolute right-0 -bottom-7'/></h1>
+      <p className='hidden md:block text-gray-500 max-w-2xl mx-auto'>We bring together world-class instructors, interactive content, and a supportive
+      community to help you achieve your personal and professional goals.</p>
+      <p className=' md:hidden max-w-sm mx-auto'>We bring together world-class instructors, interactive content, and a supportive</p>
+    </div>
   )
 }
 
